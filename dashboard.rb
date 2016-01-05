fffa51739551244f9e042a4063b5774400d4243d
philiptang42 <philiptang42@gmail.com>
2016-01-04 21:49:38 -0500

    weather

--- dashboard.rb
+++ dashboard.rb
@@ -8,6 +8,8 @@ class Dashboard < Sinatra::Base
   get("/") do
     @ip = request.ip
     @geolocation = Geolocation.new(@ip)
+
+    @weather = Weather.new(@geolocation.zip)
     erb :dashboard
   end
 end
--- lib/weather.rb
+++ lib/weather.rb
@@ -4,8 +4,26 @@ require "dotenv"
 
 Dotenv.load
 
-key = ENV["WUNDERGROUND_API_KEY"]
-
 class Weather
+  attr_reader :data
+
+  def initialize(zip_code)
+    @zip_code = zip_code
+    @data = api_call
+  end
+
+  def weather_conditions
+    conditions = @data['current observation']['weather']
+    temperature = @data['current observation']['temperature_string']
+    "#{conditions}, #{temperature}"
+  end
+
+  private
+  def api_call
+    key = ENV["WUNDERGROUND_API_KEY"]
+    uri = URI("http://api.wunderground.com/api/#{key}/conditions/q/#{@zip_code}.json")
+    response = Net::HTTP.get_response(uri)
+    JSON.parse(response.body)
+  end
 
 end
